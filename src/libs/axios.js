87a60f88d302445fe3068deab7415c362428722a
Brandon Quintero <brandonquinterogit@gmail.com>
2024-09-16 18:40:25 -0500

    Add new render variable

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-
-const render = "https://rcservice.onrender.com/api";
+const render = "https://rcservice-back.onrender.com/api";
 const localhost = "http://localhost:3000/api";
+const baseURL = import.meta.env.BACKEND_URL === "Production" ? render : localhost;
+console.log(baseURL)
 const instance = axios.create({
-  baseURL: localhost,
+  baseURL,
   withCredentials: true,
 });
 export default instance;
